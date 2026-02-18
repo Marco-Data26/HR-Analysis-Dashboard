5845cdecfa26e0288f1d8e840ff14eb3ddf2efd6
Marco Samy <117690111+Marco-Data26@users.noreply.github.com>
2026-02-18 23:53:48 +0200

    Add files via upload

---
+++ Cleaning_Queries.sql
@@ -0,0 +1,39 @@
+USE HR;
+
+SELECT COUNT (*) AS Records_Count FROM [Human Resources];
+
+SELECT * FROM [Human Resources];
+
+ALTER TABLE [Human Resources]
+ALTER COLUMN id varchar(20);
+
+EXEC sp_help [Human Resources];
+
+SELECT TRY_CONVERT(date,termdate)
+FROM [Human Resources]
+
+SELECT * FROM [Human Resources]
+WHERE termdate IS NOT NULL
+AND TRY_CONVERT(date,termdate) IS NULL
+
+UPDATE [Human Resources]
+SET termdate = LEFT(termdate,10)
+WHERE TRY_CONVERT(date, LEFT(termdate,10)) IS NOT NULL;
+
+ALTER TABLE [Human Resources]
+ALTER COLUMN termdate date;
+
+ALTER TABLE [Human Resources]
+ADD Age INT;
+
+UPDATE [Human Resources]
+SET Age = DATEDIFF(year,birthdate,'2020-12-31');
+
+ALTER TABLE [Human Resources]
+ADD Length_Of_Employment DECIMAL(4,2);
+
+UPDATE [Human Resources]
+SET Length_Of_Employment = DATEDIFF(DAY,hire_date,termdate)/365.0
+WHERE termdate IS NOT NULL;
+
+SELECT min(Age),max(Age) FROM [Human Resources];
